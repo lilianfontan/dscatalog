ecc319da08128ce073f6d8643da8260406cc0db4
lilianfontan <lilian.fontan@gmail.com>
2022-04-27 18:22:57 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/dscatalog2/Dscatalog2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscatalog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscatalog2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatalog2/Dscatalog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dscatalog2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
